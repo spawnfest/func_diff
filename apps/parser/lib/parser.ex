9bfe1e83af58c26fa531f6f7606eab3ffcf17a4e
sophyc <sophy.cao@derivco.se>
2018-11-24 17:33:55 +0100

    WIP: add end_line

--- apps/parser/lib/parser.ex
+++ apps/parser/lib/parser.ex
@@ -4,13 +4,13 @@ defmodule Parser do
   defmodule ModuleInfo do
     @type t() :: %__MODULE__{
             module_name: binary() | nil,
-            line_number: integer() | nil,
+            start_line: integer() | nil,
             functions: list(FunctionInfo.t()),
             macros: list(MacroInfo.t())
           }
     defstruct(
       module_name: nil,
-      line_number: nil,
+      start_line: nil,
       functions: [],
       macros: []
     )
@@ -19,22 +19,26 @@ defmodule Parser do
   defmodule FunctionInfo do
     @type t() :: %__MODULE__{
             function_name: binary() | nil,
-            line_number: integer() | nil
+            start_line: integer() | nil,
+            end_line: integer() | nil
           }
     defstruct(
       function_name: nil,
-      line_number: nil
+      start_line: nil,
+      end_line: nil
     )
   end
 
   defmodule MacroInfo do
     @type t() :: %__MODULE__{
             macro_name: binary() | nil,
-            line_number: integer()
+            start_line: integer(),
+            end_line: integer()
           }
     defstruct(
       macro_name: nil,
-      line_number: nil
+      start_line: nil,
+      end_line: nil
     )
   end
 
@@ -68,7 +72,7 @@ defmodule Parser do
       |> extract_functions(%ModuleInfo{})
       |> extract_macros(block)
       |> Map.put(:module_name, parent_module)
-      |> Map.put(:line_number, Keyword.get(meta, :line))
+      |> Map.put(:start_line, Keyword.get(meta, :line))
       |> extract_module_name(module)
 
     [module_info] ++ parse(block, module_info.module_name)
@@ -78,10 +82,10 @@ defmodule Parser do
   defp parse(_, _), do: []
 
   # get module name from the aliases block if on the same line as defmodule
-  defp extract_module_name(module_info = %{line_number: line_number}, [
+  defp extract_module_name(module_info = %{start_line: start_line}, [
          {:__aliases__, meta, names} | _t
        ]),
-       do: update_module_name(module_info, names, Keyword.get(meta, :line) == line_number)
+       do: update_module_name(module_info, names, Keyword.get(meta, :line) == start_line)
 
   defp extract_module_name(module_info, [_h | t]), do: extract_module_name(module_info, t)
 
@@ -93,25 +97,30 @@ defmodule Parser do
   defp extract_functions([], module_info = %{functions: functions}),
     do: %{
       module_info
-      | functions: Enum.sort(functions, fn a, b -> a.line_number < b.line_number end)
+      | functions: Enum.sort(functions, fn a, b -> a.start_line < b.start_line end)
     }
 
   defp extract_functions([{:def, meta, func_info} | t], module_info = %{functions: functions}) do
-    function = parse_function(func_info, %FunctionInfo{line_number: Keyword.get(meta, :line)})
-    extract_functions(t, %{module_info | functions: [function | functions]})
+    function = parse_function(func_info, %FunctionInfo{start_line: Keyword.get(meta, :line)})
+    extract_functions(t, %{module_info | functions: [function | update_end_line(meta, functions)]})
   end
 
   defp extract_functions([{:defp, meta, func_info} | t], module_info = %{functions: functions}) do
-    function = parse_function(func_info, %FunctionInfo{line_number: Keyword.get(meta, :line)})
-    extract_functions(t, %{module_info | functions: [function | functions]})
+    function = parse_function(func_info, %FunctionInfo{start_line: Keyword.get(meta, :line)})
+    extract_functions(t, %{module_info | functions: [function | update_end_line(meta, functions)]})
   end
 
-  defp extract_functions([_ | t], module_info), do: extract_functions(t, module_info)
+  defp extract_functions([{_, meta, _} | t], module_info = %{functions: functions}), do:
+    extract_functions(t, %{module_info|functions: update_end_line(meta, functions)})
+  defp extract_functions([_|t], module_info), do: extract_functions(t, module_info)
+
+  defp update_end_line(_meta, []), do: []
+  defp update_end_line(meta, [function|t]), do: [%{function|end_line: Keyword.get(meta, :line)}|t]
 
   defp extract_macros(module_info, []), do: module_info
 
   defp extract_macros(module_info = %{macros: macros}, [{:defmacro, meta, macro_info} | t]) do
-    macro = parse_macro(macro_info, %MacroInfo{line_number: Keyword.get(meta, :line)})
+    macro = parse_macro(macro_info, %MacroInfo{start_line: Keyword.get(meta, :line)})
     extract_macros(%{module_info | macros: [macro | macros]}, t)
   end
 
@@ -127,15 +136,15 @@ defmodule Parser do
   defp update_module_name(module_info, _names, false), do: module_info
 
   # obtain function name from the same line as def/defp
-  defp parse_function([{name, meta, _args_info} | _t], func_info = %{line_number: line_number}) do
-    case Keyword.get(meta, :line) == line_number do
+  defp parse_function([{name, meta, _args_info} | _t], func_info = %{start_line: start_line}) do
+    case Keyword.get(meta, :line) == start_line do
       true -> %{func_info | function_name: name}
       false -> func_info
     end
   end
 
-  defp parse_macro([{name, meta, _} | _t], macro_info = %{line_number: line_number}) do
-    case Keyword.get(meta, :line) == line_number do
+  defp parse_macro([{name, meta, _} | _t], macro_info = %{start_line: start_line}) do
+    case Keyword.get(meta, :line) == start_line do
       true -> %{macro_info | macro_name: Atom.to_string(name)}
       false -> macro_info
     end
