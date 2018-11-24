0868daee5b9f25e041d5d1c6454871fcedda0398
LOU Xun <aquarhead@ela.build>
2018-11-24 14:23:18 +0000

    Initial work on Runner.Diff
    with a failing test

--- apps/runner/lib/diff.ex
+++ apps/runner/lib/diff.ex
@@ -1,3 +1,32 @@
 defmodule Runner.Diff do
-  # TODO: diff 2 function bodies
+  @moduledoc "Diff function bodies."
+
+  @doc """
+  Diff function bodies, `file_prefix` is provided to run `diff`
+  in parallel without causing conflicts
+  """
+  def diff(text_a, text_b, file_prefix \\ "") do
+    dir = System.tmp_dir!()
+
+    file_a = Path.join(dir, file_prefix <> "_a")
+    file_b = Path.join(dir, file_prefix <> "_b")
+
+    File.write!(file_a, text_a <> "\n")
+    File.write!(file_b, text_b <> "\n")
+
+    result = Porcelain.exec(
+      "diff",
+      [file_a, file_b]
+    )
+
+    File.rm(file_a)
+    File.rm(file_b)
+
+    parse_result(result, text_a)
+  end
+
+  defp parse_result(%Porcelain.Result{status: 0, out: output}, base_text) do
+    # TODO
+  end
+  defp parse_result(_, _), do: []
 end
---
+++ apps/runner/test/diff_test.exs
@@ -0,0 +1,21 @@
+defmodule Runner.DiffTest do
+  use ExUnit.Case
+
+  import Runner.Diff
+
+  test "diff output" do
+    output = diff( File.read!("test/priv/text1"), File.read!("test/priv/text2") )
+
+    expected = [
+      {:add, "line0"},
+      {:common, "line1"},
+      {:common, "line2"},
+      {:del, "line3"},
+      {:add, "line4"},
+      {:common, "line5"},
+      {:del, "line6"}
+    ]
+
+    assert expected == output
+  end
+end
