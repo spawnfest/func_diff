e0787384942ca035feb5c168b62e96080329e154
LOU Xun <aquarhead@ela.build>
2018-11-25 02:26:03 +0000

    Start to add some test for Parser

--- apps/parser/test/parser_test.exs
+++ apps/parser/test/parser_test.exs
@@ -1,3 +1,20 @@
 defmodule ParserTest do
   use ExUnit.Case
+
+  import Parser, only: [process: 1]
+
+  describe "process" do
+    test "single empty module" do
+      expected = [
+        %Parser.ModuleInfo{
+          module_name: "SingleModule",
+          start_line: 1,
+          functions: [],
+          macros: []
+        }
+      ]
+
+      assert expected == process("test/priv/single_empty_module.ex")
+    end
+  end
 end
---
+++ apps/parser/test/priv/single_empty_module.ex
@@ -0,0 +1,2 @@
+defmodule SingleModule do
+end
