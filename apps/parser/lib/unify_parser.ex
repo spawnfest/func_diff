f384df1fb76489b5b42406f066fe1eff46688e97
LOU Xun <aquarhead@ela.build>
2018-11-25 13:32:24 +0000

    Add def parsing and entry

--- apps/parser/lib/unify_parser.ex
+++ apps/parser/lib/unify_parser.ex
@@ -1,14 +1,16 @@
 defmodule Parser.Unify do
-  alias Parser.ModuleInfo
+  defmodule ModuleInfo do
+    defstruct [:name, :end_line, :defs]
+  end
 
   defmodule Defined do
     @type t() :: %__MODULE__{
-      name: binary() | nil,
-      start_line: integer() | nil,
-      end_line: integer() | nil,
-      arity: integer() | nil,
-      private?: boolean()
-    }
+            name: binary() | nil,
+            start_line: integer() | nil,
+            end_line: integer() | nil,
+            arity: integer() | nil,
+            private?: boolean()
+          }
     defstruct(
       name: nil,
       start_line: nil,
@@ -16,6 +18,33 @@ defmodule Parser.Unify do
       arity: nil,
       private?: false
     )
+  end
+
+  def entry(file) do
+    file_content = File.read!(file)
+    file_lines = String.split(file_content, "\n")
+
+    root_ast = Code.string_to_quoted!(file_content)
+
+    case root_ast do
+      {:defmodule, meta,
+       [
+         {:__aliases__, meta, [module_name]},
+         [do: do_stuff]
+       ]} ->
+        root_module = %ModuleInfo{
+          name: module_name,
+          end_line: find_nonempty_line_before(file_lines, length(file_lines)),
+          defs: []
+        }
+
+        blocks = list_do_block(do_stuff)
+
+        parse([], blocks, nil, root_module, [])
+
+      other ->
+        IO.inspect(other)
+    end
   end
 
   # parse(modules_left, block_left, current_defined, current_module, modules_acc)
@@ -32,41 +61,152 @@ defmodule Parser.Unify do
 
   # module parsed
   def parse(modules_left, [], nil, current_module, modules_acc) do
-    parse(modules_left, [], nil, nil, [ current_module | modules_acc ])
+    parse(modules_left, [], nil, nil, [current_module | modules_acc])
   end
 
   # last `Defined` in a module
   def parse(modules_left, [], %Defined{} = df, current_module, modules_acc) do
     end_df = %{df | end_line: current_module.end_line - 1}
 
-    new_module = %{current_module | functions: [end_df | current_module.functions]}
-
+    new_module = save_defined(end_df, current_module)
     parse(modules_left, [], nil, new_module, modules_acc)
   end
 
   # @doc starts a new `current_df`
-  def parse(modules_left, [{:@, [line: l], [{:doc, _, _}]} | block_left], nil, current_module, modules_acc) do
+  def parse(
+        modules_left,
+        [{:@, [line: l], [{:doc, _, _}]} | block_left],
+        nil,
+        current_module,
+        modules_acc
+      ) do
+    new_df = %Defined{start_line: l}
 
+    parse(modules_left, block_left, new_df, current_module, modules_acc)
   end
 
-  def parse(modules_left, [{:@, [line: l], [{:doc, _, _}]} | block_left], current_df, current_module, modules_acc) do
-  end
+  def parse(
+        modules_left,
+        [{:@, [line: l], [{:doc, _, _}]} | block_left],
+        current_df,
+        current_module,
+        modules_acc
+      ) do
+    end_df = %{current_df | end_line: l - 1}
+    new_module = save_defined(end_df, current_module)
 
+    new_df = %Defined{start_line: l}
+
+    parse(modules_left, block_left, new_df, new_module, modules_acc)
+  end
 
   # @spec continues or starts a new `Defined`
+  def parse(
+        modules_left,
+        [{:@, [line: l], [{:spec, _, _}]} | block_left],
+        nil,
+        current_module,
+        modules_acc
+      ) do
+    new_df = %Defined{start_line: l}
 
+    parse(modules_left, block_left, new_df, current_module, modules_acc)
+  end
+
+  def parse(
+        modules_left,
+        [{:@, [line: l], [{:spec, _, spec_b}]} | block_left],
+        current_df,
+        current_module,
+        modules_acc
+      ) do
+    # TODO: check function name?
+    # [{:::, _, [{df_name, _, _} | _]}]} = spec_b
+    case current_df.name do
+      nil ->
+        # continue from @doc
+        parse(modules_left, block_left, current_df, current_module, modules_acc)
+
+      _ ->
+        # function is named, ends `current_df` and start a new one
+        end_df = %{current_df | end_line: l - 1}
+        new_module = save_defined(end_df, current_module)
+
+        new_df = %Defined{start_line: l}
+
+        parse(modules_left, block_left, new_df, new_module, modules_acc)
+    end
+  end
 
   # def, defp, defmacro, defmacrop continues or starts a new `Defined`
+  def parse(
+        modules_left,
+        [{:def, [line: l], [{df_name, _, args} | _]} | block_left],
+        nil,
+        current_module,
+        modules_acc
+      ) do
+    new_df = %Defined{
+      name: df_name,
+      start_line: l,
+      private?: false,
+      arity: count_args(args)
+    }
 
+    parse(modules_left, block_left, new_df, current_module, modules_acc)
+  end
+
+  def parse(
+        modules_left,
+        [{:def, [line: l], [{df_name, _, args} | _]} | block_left],
+        current_df,
+        current_module,
+        modules_acc
+      ) do
+    case current_df.name do
+      ^df_name ->
+        # same function name, continue
+        parse(modules_left, block_left, current_df, current_module, modules_acc)
+
+      _ ->
+        # different name, starting a new `Defined`
+        end_df = %{current_df | end_line: l - 1}
+        new_module = save_defined(end_df, current_module)
+
+        new_df = %Defined{
+          name: df_name,
+          start_line: l,
+          private?: false,
+          arity: count_args(args)
+        }
+
+        parse(modules_left, block_left, new_df, new_module, modules_acc)
+    end
+  end
 
   # defmodule updates modules_left
 
-
-
-
-
-
-
   ## private helpers
-  defp get_line(meta), do: Keyword.get(meta, :line)
+  defp save_defined(df, module) do
+    case df.name do
+      # incomplete definition
+      nil -> module
+      _ -> %{module | defs: [df | module.defs]}
+    end
+  end
+
+  defp count_args(nil), do: 0
+  defp count_args(args), do: length(args)
+
+  defp list_do_block({:__block__, [], block}), do: block
+  defp list_do_block(single_block), do: [single_block]
+
+  defp find_nonempty_line_before(lines, lineno) do
+    this_line = lines |> Enum.at(lineno - 1) |> String.trim()
+
+    case this_line do
+      "" -> find_nonempty_line_before(lines, lineno - 1)
+      _ -> lineno
+    end
+  end
 end
