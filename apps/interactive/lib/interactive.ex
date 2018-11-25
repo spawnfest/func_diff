51c9b1c4216e72f271f976d385673e0d94069445
LOU Xun <aquarhead@ela.build>
2018-11-25 17:53:04 +0000

    Pad diff output, also prepare other entry functions

--- apps/interactive/lib/interactive.ex
+++ apps/interactive/lib/interactive.ex
@@ -24,10 +24,33 @@ defmodule Interactive do
     %Token{pid: pid}
   end
 
-  def modules(%Token{} = _token) do
+  @doc """
+  List differences at modules (project) level
+  """
+  def modules(%Token{} = token) do
+    token.pid
+    |> CA.get_state()
+    |> Map.get(:modules_diff)
+    |> print_diff()
   end
 
   def modules(_), do: {:error, :invalid_token}
+
+  @doc """
+  List differences at a single module's level
+  """
+  def module(%Token{} = _token, module) do
+  end
+
+  def module(_, _), do: {:error, :invalid_token}
+
+  @doc """
+  List diff of a single function/macro
+  """
+  def func(%Token{} = _token, module, func) do
+  end
+
+  def func(_, _, _), do: {:error, :invalid_token}
 
   ## debug helpers
 
@@ -51,13 +74,20 @@ defmodule Interactive do
   defp print_diff(diff) do
     Enum.each(diff, fn
       {:add, line} ->
-        IO.ANSI.format([:light_green_background, :black, "+ " <> line]) |> IO.puts()
+        IO.ANSI.format([:light_green_background, :black, deco_line("+", line)]) |> IO.puts()
 
       {:del, line} ->
-        IO.ANSI.format([:light_black_background, :light_cyan, "- " <> line]) |> IO.puts()
+        IO.ANSI.format([:light_black_background, :light_cyan, deco_line("-", line)]) |> IO.puts()
+
+      # TODO: changed, "! "
 
       {:common, line} ->
-        IO.puts("  " <> line)
+        deco_line(" ", line) |> IO.puts()
     end)
+  end
+
+  defp deco_line(deco, line, pad \\ 80) do
+    (deco <> " " <> line)
+    |> String.pad_trailing(pad)
   end
 end
